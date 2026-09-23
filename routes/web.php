91e72df017d6f3d3c6d8fa50cf259d8d403f5fe2
sleepyashell <135983000+sleepyashell@users.noreply.github.com>
2026-09-24 02:16:42 +0700

    feat: implement landing page UI

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'landing')->name('home');
 
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
